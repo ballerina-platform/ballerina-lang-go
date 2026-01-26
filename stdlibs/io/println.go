ba9aba55a90fa5ca890168d658c92a580d005882
warunalakshitha <warudanu@gmail.com>
2026-01-26 14:18:06 +0530

    Add standard library io module

---
+++ stdlibs/io/println.go
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2026, WSO2 LLC. (http://www.wso2.com).
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package io
+
+import (
+	"ballerina-lang-go/runtime/api"
+	"fmt"
+)
+
+func init() {
+	ioModule := api.RegisterNativeModule("ballerina", "io")
+	api.RegisterExternFunction(ioModule, "println", func(args []any) (any, error) {
+		Println(args...)
+		return nil, nil
+	})
+}
+
+func Println(values ...any) {
+	for i, v := range values {
+		if i > 0 {
+			fmt.Print(" ")
+		}
+		fmt.Print(v)
+	}
+	fmt.Println()
+}
