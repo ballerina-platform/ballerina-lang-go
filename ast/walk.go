1ccc0e4aef5ee7828aea1d758a7c6e3216f236dd
Heshan Padamsiri <hpheshan@gmail.com>
2026-03-10 15:43:56 +0530

    Implement intersection type support

--- ast/walk.go
+++ ast/walk.go
@@ -599,6 +599,10 @@ func Walk(v Visitor, node BLangNode) {
 		WalkTypeData(v, &node.lhs)
 		WalkTypeData(v, &node.rhs)
 
+	case *BLangIntersectionTypeNode:
+		WalkTypeData(v, &node.lhs)
+		WalkTypeData(v, &node.rhs)
+
 	case *BLangErrorTypeNode:
 		WalkTypeData(v, &node.DetailType)
 
