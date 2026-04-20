92f73f58a8a5c97fc5898ea6ba0522ec5140b0df
warunalakshitha <warudanu@gmail.com>
2026-04-20 15:32:59 +0530

    Add tests for float equality and inequality

--- corpus/integration_test.go
+++ corpus/integration_test.go
@@ -60,9 +60,7 @@ var (
 	update = flag.Bool("update", false, "update corpus integration test outputs")
 
 	// Skip tests that cause unrecoverable Go runtime errors
-	skipIntegrationTests = []string{
-		"subset8/08-const/const3-v.bal",
-	}
+	skipIntegrationTests []string
 )
 
 func TestMain(m *testing.M) {
