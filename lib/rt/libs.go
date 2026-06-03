e4d9ead215122c21b8fb69f71c33f46a0443c9c0
Heshan Padamsiri <hpheshan@gmail.com>
2026-06-03 07:29:38 +0530

    refactor(langlib): move migrated langlib/stdlib go logic under langlibs/stdlibs/go

--- lib/rt/libs.go
+++ lib/rt/libs.go
@@ -18,11 +18,11 @@ package rt
 
 import (
 	_ "ballerina-lang-go/lib/array/runtime"
-	_ "ballerina-lang-go/lib/error/runtime"
-	_ "ballerina-lang-go/lib/int/runtime"
 	_ "ballerina-lang-go/lib/langinternal/runtime"
+	_ "ballerina-lang-go/lib/langlibs/go/lang.error"
+	_ "ballerina-lang-go/lib/langlibs/go/lang.int"
+	_ "ballerina-lang-go/lib/langlibs/go/lang.string"
 	_ "ballerina-lang-go/lib/map/runtime"
-	_ "ballerina-lang-go/lib/string/runtime"
 
 	// standard libraries
 	_ "ballerina-lang-go/lib/stdlibs/ballerina/http/0.0.1/go1.2/native"
